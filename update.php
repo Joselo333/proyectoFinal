70dd24d17f308c27ad704f704d8aa18ad4138c87
Agust-Ast <agtudillo@gmail.com>
2021-12-19 13:01:54 -0300

    Se agrega update.php

---
+++ update.php
@@ -0,0 +1,24 @@
+<?php
+
+include("conexion.php");
+$con=conectar();
+
+$nombre=$_POST['nombre'];
+$apellidos=$_POST['apellidos'];
+$rut=$_POST['rut'];
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+$direccion=$_POST['direccion'];
+$sexo=$_POST['sexo'];
+$nacimiento=$_POST['nacimiento'];
+$edad=$_POST['edad'];
+$email=$_POST['email'];
+$ID=$_POST['ID'];
+
+$sql="UPDATE formulario SET  rut='$rut',nombre='$nombre',usuario='$usuario',direccion='$direccion',email='$email',edad='$edad',apellidos='$apellidos' WHERE ID='$ID'";
+$query=mysqli_query($con,$sql);
+
+    if($query){
+        Header("Location: buscar.php");
+    }
+?>
